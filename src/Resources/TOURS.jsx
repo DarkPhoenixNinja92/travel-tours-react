a4f3d10db3fa10471d1397d704bbb6eacd14d7e8
Shaun <scpour@gmail.com>
2023-08-09 09:54:27 -0400

    first commit

---
+++ src/Resources/TOURS.jsx
@@ -0,0 +1,65 @@
+export const TOURS = [
+    {
+        id: 0,
+        title: 'Best Of Paris In 7 Days Tour',
+        text: "Paris is synonymous with the finest things that culture can offer — in art, fa" +
+                "shion, food, literature, and ideas. On this tour, your Paris-savvy Rick Steves" +
+                " guide will immerse you in the very best of the City of Light: the masterpiece" +
+                "-packed Louvre and Orsay museums, resilient Notre-Dame Cathedral, exquisite Sa" +
+                "inte-Chapelle, and extravagant Palace of Versailles. You'll also enjoy guided " +
+                "neighborhood walks through the city's historic heart as well as quieter moment" +
+                "s to slow down and savor the city's intimate cafés, colorful markets, and joie" +
+                " de vivre. Join us for the Best of Paris in 7 Days!",
+        price: 1995,
+        img: ''
+    }, {
+        id: 1,
+        title: 'Best Of Ireland In 14 Days Tour',
+        text: "Rick Steves' Best of Ireland tour kicks off with the best of Dublin, followed " +
+                "by Ireland's must-see historical sites, charming towns, music-filled pubs, and" +
+                " seaside getaways — including Kinsale, the Dingle Peninsula, the Cliffs of Moh" +
+                "er, the Aran Islands, Galway, Connemara, Giant's Causeway, and the compelling " +
+                "city of Belfast. All along the way, Rick's guides will share their stories to " +
+                "draw you in to the Emerald Isle, and the friendliness of the people will surel" +
+                "y steal your heart. Join us for the Best of Ireland in 14 Days!",
+        price: 3895,
+        img: ''
+    }, {
+        id: 2,
+        title: 'Best Of Salzburg & Vienna In 8 Days Tour',
+        text: "Let's go where classical music, towering castles, and the-hills-are-alive scen" +
+                "ery welcome you to the gemütlichkeit of Bavaria and opulence of Austria's Gold" +
+                "en Age. Your Rick Steves guide will bring this region's rich history and cultu" +
+                "re to life in festive Munich, Baroque Salzburg, sparkling Lake Hallstatt, mona" +
+                "stic Melk, the blue Danube, and royal Vienna — with cozy villages and alpine v" +
+                "istas all along the way. Join us for the Best of Munich, Salzburg & Vienna in " +
+                "8 Days!",
+        price: 2695,
+        img: ''
+    }, {
+        id: 3,
+        title: 'Best Of Rome In 7 Days Tour',
+        text: "Our Rome tour serves up Europe's most intoxicating brew of dazzling art, earth" +
+                "-shaking history, and city life with style. On this Rome vacation, your tour g" +
+                "uide will resurrect the grandeur of ancient Rome's Colosseum, Forum, Pantheon," +
+                " and nearby Ostia Antica. From the Renaissance and Baroque eras, you'll marvel" +
+                " at St. Peter's Basilica, the Vatican Museums, Sistine Chapel, and Borghese Ga" +
+                "llery. You'll also enjoy today's Rome, with neighborhood walking tours, memora" +
+                "ble restaurants, and time to explore on your own. Join us for the Best of Rome" +
+                " in 7 Days!",
+        price: 2095,
+        img: ''
+    }, {
+        id: 4,
+        title: 'Best Of Poland In 10 Days Tour',
+        text: "Starting in the colorful port city of Gdańsk, you'll escape the crowds and emb" +
+                "race the understated elegance of ready-for-prime-time Poland for 10 days. With" +
+                " an expert Rick Steves guide at your side, you'll experience mighty Malbork ca" +
+                "stle, the cobbly-cute village of Toruń, Poland's contemporary capital of Warsa" +
+                "w, the spiritual Jasna Góra Monastery, and charming Kraków — Poland's finest c" +
+                "ity. In this land of surprises — so trendy and hip, yet steeped in history — t" +
+                "here's so much to discover. Join us for the Best of Poland in 10 Days!",
+        price: 2595,
+        img: ''
+    }
+]
